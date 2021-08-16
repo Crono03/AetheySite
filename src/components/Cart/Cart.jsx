9b3e65d772f2a02abbcc1f5de949a6944ef7727c
Crono03 <>
2021-08-16 18:03:38 +0200

    Cart basic

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Typography, Button, Grid } from '@material-ui/core';
+
+
+const Cart = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Cart
