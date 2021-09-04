7269027b60196afdd3650c2ffdf380eabcf0bbd3
Crono03 <>
2021-09-04 19:00:13 +0200

    negro estamos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
-import React from 'react'
-
-const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+import React,{ Component }  from 'react';
+ 
+class App extends Component {
+ 
+  render() {
+    return (    
+       <div>
+         <h1>Negro siamo nelle trenches negro</h1>
+       </div>
+ 
+    );
+  }
 }
-
-export default App
-
+    
+export default App;
